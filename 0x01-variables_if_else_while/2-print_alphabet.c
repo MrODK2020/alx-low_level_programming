d3b86a1563c445d4a7087262218f3f118fe92258
OLUWASAKIN OLUWADUNSIN <oluwasakindunsin@gmail.com>
2022-04-09 02:49:44 -0700

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,16 +9,15 @@
  */
 
 int main(void)
-{		
+{
 	int i = 97;
 
-	while (i < 123)		
+	while (i < 123)
 	{
-	        putchar(i);
+		putchar(i);
 		i++;
 	}
-
-	putchar(10);			
+	putchar(10);
 
 	return (0);
 }
